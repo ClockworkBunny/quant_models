879c664766fde15284c83c3ce58f3b5cc3446c64
rz0718 <rui91seu@gmail.com>
2019-12-31 13:35:29 +0800

    modify env logis by addresssing zero returns

--- portfolio/environment.py
+++ portfolio/environment.py
@@ -8,9 +8,18 @@ class EquityEnvironment:
         self.data    = inputdf
 
     def preprocess_state(self, state):
-        return state
+        """add the logic that remove the all-zero returns"""
+        zero_cols = (state == 0).all()
+        new_state  = state.loc[:,~zero_cols]
+        if new_state.shape[1] != state.shape[1]:
+            print('remove the zero returns of stocks')
+            print(set(state.columns.tolist()) - set(new_state.columns.tolist()))
+            removal_label = True
+        else:
+            removal_label  = False
+        return new_state, removal_label
 
-    def get_state(self, t, lookback, is_cov_matrix = True, is_raw_time_series = False):
+    def get_state(self, t, lookback, is_cov_matrix = True, is_raw_time_series = False, is_fit = True):
         """
         # args:
         1. t: the ending lindex,  t-lookback: the start index
@@ -19,6 +28,9 @@ class EquityEnvironment:
         3. is_raw_time_series: boolean.
         true indicates the input df is the return series
         flase indicates the input df is the price series
+        4. is_fit:
+        true indicates the input df is for weights computation, which do not allow for all zero returns i.e. whole columne
+        else indicates means no filter
         """
         assert lookback <= t
         decision_making_state = self.data.iloc[t-lookback:t]
@@ -26,11 +38,14 @@ class EquityEnvironment:
 
         if is_cov_matrix:
             x = decision_making_state.cov()
-            return x
+            return x, False
         else:
             if is_raw_time_series:
                 decision_making_state = self.data.iloc[t-lookback:t]
+        if is_fit:
             return self.preprocess_state(decision_making_state)
+        else:
+            return decision_making_state, False
 
     def get_reward(self, action, action_t, reward_t, alpha = 0.01):
         def local_portfolio(returns, weights):
